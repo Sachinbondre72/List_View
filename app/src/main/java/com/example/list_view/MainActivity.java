de63f8acb76b46556d8c7fb74cd2ee8fee99e62c
Sachin Bondre <Sachinbondre72@gmail.com>
2020-02-06 09:42:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/list_view/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.list_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String string[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list1);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,string);
+        listView.setAdapter(arrayAdapter);
+    }
+}
